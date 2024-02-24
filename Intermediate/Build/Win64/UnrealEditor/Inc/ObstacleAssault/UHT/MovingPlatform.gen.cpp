301b18ab10a6e205fc695405eb5493f83af65bab
seoloo <83264303+seoloo@users.noreply.github.com>
2024-02-24 20:53:40 +0900

    초기 커밋.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ObstacleAssault/UHT/MovingPlatform.gen.cpp
@@ -0,0 +1,110 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "ObstacleAssault/MovingPlatform.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
+// Cross Module References
+	ENGINE_API UClass* Z_Construct_UClass_AActor();
+	OBSTACLEASSAULT_API UClass* Z_Construct_UClass_AMovingPlatform();
+	OBSTACLEASSAULT_API UClass* Z_Construct_UClass_AMovingPlatform_NoRegister();
+	UPackage* Z_Construct_UPackage__Script_ObstacleAssault();
+// End Cross Module References
+	void AMovingPlatform::StaticRegisterNativesAMovingPlatform()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(AMovingPlatform);
+	UClass* Z_Construct_UClass_AMovingPlatform_NoRegister()
+	{
+		return AMovingPlatform::StaticClass();
+	}
+	struct Z_Construct_UClass_AMovingPlatform_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_My_Int_MetaData[];
+#endif
+		static const UECodeGen_Private::FIntPropertyParams NewProp_My_Int;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMovingPlatform_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AActor,
+		(UObject* (*)())Z_Construct_UPackage__Script_ObstacleAssault,
+	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::DependentSingletons) < 16);
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMovingPlatform_Statics::Class_MetaDataParams[] = {
+		{ "IncludePath", "MovingPlatform.h" },
+		{ "ModuleRelativePath", "MovingPlatform.h" },
+	};
+#endif
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMovingPlatform_Statics::NewProp_My_Int_MetaData[] = {
+		{ "Category", "MovingPlatform" },
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "// \xef\xbf\xbd\xc6\xb7\xef\xbf\xbd \xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd \xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xcd\xbf\xef\xbf\xbd \xef\xbf\xbd\xcb\xb8\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\n" },
+#endif
+		{ "ModuleRelativePath", "MovingPlatform.h" },
+#if !UE_BUILD_SHIPPING
+		{ "ToolTip", "\xef\xbf\xbd\xc6\xb7\xef\xbf\xbd \xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd \xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd\xcd\xbf\xef\xbf\xbd \xef\xbf\xbd\xcb\xb8\xef\xbf\xbd\xef\xbf\xbd\xef\xbf\xbd" },
+#endif
+	};
+#endif
+	const UECodeGen_Private::FIntPropertyParams Z_Construct_UClass_AMovingPlatform_Statics::NewProp_My_Int = { "My_Int", nullptr, (EPropertyFlags)0x0010000000000001, UECodeGen_Private::EPropertyGenFlags::Int, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AMovingPlatform, My_Int), METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::NewProp_My_Int_MetaData), Z_Construct_UClass_AMovingPlatform_Statics::NewProp_My_Int_MetaData) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AMovingPlatform_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AMovingPlatform_Statics::NewProp_My_Int,
+	};
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMovingPlatform_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMovingPlatform>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_AMovingPlatform_Statics::ClassParams = {
+		&AMovingPlatform::StaticClass,
+		"Engine",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		Z_Construct_UClass_AMovingPlatform_Statics::PropPointers,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::PropPointers),
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::Class_MetaDataParams), Z_Construct_UClass_AMovingPlatform_Statics::Class_MetaDataParams)
+	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::PropPointers) < 2048);
+	UClass* Z_Construct_UClass_AMovingPlatform()
+	{
+		if (!Z_Registration_Info_UClass_AMovingPlatform.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_AMovingPlatform.OuterSingleton, Z_Construct_UClass_AMovingPlatform_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_AMovingPlatform.OuterSingleton;
+	}
+	template<> OBSTACLEASSAULT_API UClass* StaticClass<AMovingPlatform>()
+	{
+		return AMovingPlatform::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMovingPlatform);
+	AMovingPlatform::~AMovingPlatform() {}
+	struct Z_CompiledInDeferFile_FID_Users_seolh_Documents_Unreal_Projects_ObstacleAssault_Source_ObstacleAssault_MovingPlatform_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_seolh_Documents_Unreal_Projects_ObstacleAssault_Source_ObstacleAssault_MovingPlatform_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_AMovingPlatform, AMovingPlatform::StaticClass, TEXT("AMovingPlatform"), &Z_Registration_Info_UClass_AMovingPlatform, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMovingPlatform), 3271874754U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_seolh_Documents_Unreal_Projects_ObstacleAssault_Source_ObstacleAssault_MovingPlatform_h_2805000371(TEXT("/Script/ObstacleAssault"),
+		Z_CompiledInDeferFile_FID_Users_seolh_Documents_Unreal_Projects_ObstacleAssault_Source_ObstacleAssault_MovingPlatform_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_seolh_Documents_Unreal_Projects_ObstacleAssault_Source_ObstacleAssault_MovingPlatform_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
